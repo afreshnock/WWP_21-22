5d083130f9299d6f22558f6bd3f45cafda620ebd
AndrewDulac <andrew.dulac4@gmail.com>
2022-02-07 18:27:18 -0600

    added a file for the turbine and RPM

---
+++ Turbine/RPM.h
@@ -0,0 +1,68 @@
+#define Sensor 2
+
+volatile unsigned long Htime1, Htime2;
+int Raw_RPM, Avg_RPM;
+unsigned long Timer;
+int SampleInterval = 100;
+int outputRPM = 0;
+unsigned long TempStore = 0;
+bool FirstRead = true;
+unsigned long Saved_RPM [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+unsigned long RPMSum = 0;
+int i = 0;
+int F = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Sensor, INPUT);
+  Timer = millis();
+  attachInterrupt(digitalPinToInterrupt(Sensor), RPMRead, RISING); //run ISR on rising edge
+  interrupts();
+}
+
+int RPMRead() {
+  TempStore = micros();
+  if (digitalRead(Sensor) == HIGH) {
+    if (FirstRead) {
+      Htime1 = TempStore; //capture first rising edge time
+      FirstRead = false;
+    }
+    else {
+      Htime2 = TempStore; //capture second rising edge time
+      FirstRead = true;
+      noInterrupts();
+      Raw_RPM = 15000000 / (Htime2 - Htime1); //calculate rpm from period
+      if (Raw_RPM < 10000) {
+        if (abs(Raw_RPM - outputRPM) < (SampleInterval * 3)) { //compare new rpm to previous
+          if ( i < 9 ) { //cycle through array
+            i++;
+          }
+          else {
+            i = 0;
+          }
+          outputRPM = Raw_RPM; //set comparative value
+          RPMSum = RPMSum - Saved_RPM [i]; //subtract oldest value from sum
+          Saved_RPM [i] = Raw_RPM; //insert new value into array
+          RPMSum = RPMSum + Raw_RPM; //add new value to sum
+          Avg_RPM = (RPMSum / 10); 
+          Serial.println(Avg_RPM);
+          F = 0; //reset number of fails
+        }
+        else {
+          F ++; //increment failed compared value
+          if ( F > 10 ) {
+            outputRPM = Raw_RPM; //reset the comparative value
+          }
+        }
+      }
+    }
+  }
+}
+
+
+void loop() {
+  if ((millis() - Timer) > SampleInterval) {
+    interrupts();
+    Timer = millis();
+  }
+}
